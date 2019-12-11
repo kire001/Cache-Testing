be65f8ce740de0b7d0679a8371569a4945c73287
Erik Šiffel <Erik.Siffel@KornFerry.com>
2019-12-11 14:34:48 +0100

    fix

--- Parameter.js
+++ Parameter.js
@@ -1,7 +1,8 @@
 //Parameter file
 class Parameter{
-var neco = UserId + ': svet za dvojteckou';
-Spells.SaveParameter (state, report, 'heroName', UserId, neco);
-
+static function testParameter(state,report,userid){
+var neco = userid + ': svet za dvojteckou';
+Spells.SaveParameter (state, report, 'heroName', userid, neco);
+}
 }
 //static function SaveParameter (state, report, param_name, key, value) {
