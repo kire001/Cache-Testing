1039416be9ebe81d8fb84a67b6c444c7c6bfda74
Erik Šiffel <Erik.Siffel@KornFerry.com>
2019-12-11 13:05:21 +0100

    update

--- Parameter.js
+++ Parameter.js
@@ -1,6 +1,7 @@
 //Parameter file
 class Parameter{
-
-
+var neco = UserId + ': svet za dvojteckou';
+SaveParameter (state, report, 'heroName', UserId, neco);
 
 }
+//static function SaveParameter (state, report, param_name, key, value) {
