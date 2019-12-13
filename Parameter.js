44bd10818ae68ed7a849e44d6b94a710856eabe9
Erik Šiffel <Erik.Siffel@KornFerry.com>
2019-12-13 12:30:13 +0100

    ee

--- Parameter.js
+++ Parameter.js
@@ -3,7 +3,8 @@ class ParamTest{
 static function testParameter(state,report,userid){
 var neco = userid + ': svet za dvojteckou';
 Spell.Save (state, report, 'heroName', neco);
-Spell.Save (state, report, 'heroName', neco);
+Spell.LoadParameter (report, 'heroName', 'Possible Value 1');
+Spell.LoadParameter (report, 'heroName', 'Possible Value 2');
 }
 }
 //static function SaveParameter (state, report, param_name, key, value) {
